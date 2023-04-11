e5c3a3d8852c2e6b3b9353406a7b17d43c8fcd9a
pcbennett108 <pcbennett42@gmail.com>
2023-04-11 13:22:34 -0600

    Add 4 puts statements

--- variables.rb
+++ variables.rb
@@ -5,13 +5,17 @@ days_in_week = 7
 one_dozen = 12
 smiles_per_mile = 99
 pie = 3.14
-one_half = .5
+one_half = 0.5
 number_of_giggawatts = 1.21
 is_sunny_today = true
 pizza_is_bad = false
 music_is_great = true
 # variable reassignments
 one_dozen = 24
-food = "Burger"
+food = "Burgers"
 name = "Neo"
 is_sunny_today = false
+puts name
+puts "#{food} are the best thing to eat on a #{day}"
+puts pie
+puts "#{name} wants #{one_dozen} pies."
